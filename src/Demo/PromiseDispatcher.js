d9f0450472bb5cb9735b65c97a9a5047720f9ede
hh54188 <252438823@qq.com>
2019-01-06 01:04:07 +0800

    update

--- src/Demo/PromiseDispatcher.js
+++ src/Demo/PromiseDispatcher.js
@@ -1,6 +1,91 @@
 import _ from "lodash";
 
-class PromiseDispatcher {
-  constructor(tasks) {}
-  execute() {}
+const utils = {
+  // https://stackoverflow.com/questions/27746304/how-do-i-tell-if-an-object-is-a-promise#answer-27746324
+  isPromiseObj: function (obj) {
+    return obj.then && _.isFunction(obj.then)
+  }
 }
+
+export default class PromiseDispatcher {
+  constructor(tasks) {
+    this.tasks = _.isArray(tasks) ? tasks : [tasks]
+    this.execute()
+  }
+  execute() {
+    const finalPromise = this.tasks.reduce((prevPromise, curTask) => {
+      return prevPromise.then(prevResult => {
+        // ===== Async Function Compatible =====
+        let curPromise = null
+
+        try {
+          curPromise = curTask()
+        } catch(error) {
+          curPromise = Promise.reject(curPromise)
+        }
+
+        if (!utils.isPromiseObj(curPromise)) {
+          curPromise = Promise.resolve(curPromise)
+        }          
+        // =====
+        return curPromise.then(curResult => {
+          console.log('RESOLVED:)')
+          return [...prevResult, curResult]
+        }).catch(curError => {
+          console.log('REJECTED:(')
+          return [...prevResult, curError]
+        })
+      })
+    }, Promise.resolve([]))
+
+    finalPromise.then(result => {
+      console.log('result------>', result)
+    })
+  }
+}
+
+const fakeSuccessAsyncTask = function() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('------> SUCCESS ASYNC FUNCTION')
+      resolve([])
+    }, 1000 * 1)
+  })
+}
+
+const fakeFailedAsyncTask = function() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('------> FAILED ASYNC FUNCTION')
+      reject([])
+    }, 1000 * 1)
+  })  
+}
+
+
+const fakeSuccessSyncTask = function() {
+  console.log('------> SUCCESS SYNC FUNTION')
+  return []
+}
+
+const fakeFailedSyncTask = function() {
+  console.log('------> FAILED SYNC FUNTION')
+  return new Error()
+}
+
+const dispatcherInstance = new PromiseDispatcher([
+  // fakeSuccessAsyncTask,
+  // fakeSuccessAsyncTask,
+  
+  fakeFailedAsyncTask,
+  fakeFailedAsyncTask,
+
+  // fakeSuccessSyncTask,
+  // fakeSuccessSyncTask,
+  
+  // fakeFailedSyncTask,
+  // fakeFailedSyncTask,
+  
+  // fakeSuccessAsyncTask,
+  // fakeFailedAsyncTask,
+])
--- src/Demo/index.js
+++ src/Demo/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import axios from "axios";
 
+import PromiseDispatcher from './PromiseDispatcher'
+
 export default class Demo extends React.Component {
   constructor(props) {
     super(props);
   }
   componentDidMount() {
-    axios.get("http://127.0.0.1:9090/api/").then(response => {
-      console.log(response);
-    });
+    // axios.get("http://127.0.0.1:9090/api/").then(response => {
+    //   console.log(response);
+    // });
   }
   render() {
     return <div>DEMO</div>;
