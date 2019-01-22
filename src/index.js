199921209f1197dfe9706918b79c4dc5e20fe67b
hh54188 <252438823@qq.com>
2019-01-22 17:49:50 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "antd/dist/antd.less";
 
-// import App from "./App";
-import App from "./OptimizedApp_Basic";
+import App from "./App";
+// import App from "./OptimizedApp_Basic";
 // import App from "./LongTask_TTI/interactive";
 // import App from "./LongTask_TTI/load";
+
+// import ttiPolyfill from "tti-polyfill";
+// ttiPolyfill.getFirstConsistentlyInteractive().then(tti => {
+//   // Use `tti` value in some way.
+//   console.log(tti);
+// });
 
 ReactDOM.render(<App />, document.querySelector("#app"));
